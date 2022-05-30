a03e7913a77fc14cca60cc6b2111a8e74a2cb3e3
Connor Abernethy <connor.abernethy@vincit.com>
2022-05-29 18:42:28 -0700

    Update home page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4f76f9dd3878b343"; }
+/******/ 		__webpack_require__.h = function() { return "5d608f876134bbc0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import { Box, Container } from "@chakra-ui/react"
+import { Box, Container, Heading } from "@chakra-ui/react"
 
 const Page = () => {
     return (
         <Container>
             <Box borderRadius="lg" bg='lavender' p={3} mb={6} align="center">
                 Hi, I&apos;m a full-stack developer from Arizona!
+            </Box>
+            <Box display={{md: 'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Connor Abernethy
+                    </Heading>
+                    <p>Developer, Creator, and Digital Connoisseur</p>
+                </Box>
             </Box>
         </Container>
     )
